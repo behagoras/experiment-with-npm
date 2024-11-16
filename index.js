f19686590a78a3b1329d3c4a6af2286bbbc406d7
David Behar Lombrozo <david.behar.lombrozo@autodesk.com>
2024-11-16 13:52:03 -0600

    install axios

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import _ from "lodash";
+import axios from "axios";
 
 console.log(_.isEmpty({})); // Debería imprimir true
+
+
+axios.get("https://api.github.com")
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
